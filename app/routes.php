d0c8ec8920ccbfe52add0f8763bf8a89141800f4
Blas Fernandez Segura <burasu@JARVIS-Air.local>
2014-11-02 13:42:37 +0100

    Inicio del curso, de momento solo va la instalación y el enrulado

---
+++ app/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+
+Route::get('/saludo', function()
+{
+    echo "Hola desde la ruta saludo";
+});
+
+Route::get('nombre/{nombre?}', function($nombre = null)
+{
+    echo "Hola $nombre";
+});
