33c39732ecd2731537abe6407d8f9f31755545bc
Blas Fernandez Segura <burasu@JARVIS-Air.local>
2014-11-09 12:53:31 +0100

    Pruebas de uso de Seeders en Laravel

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,94 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call("UserTableSeeder");
+
+        $this->command->info("Tabla users cargada");
+
+		$this->call("PostTableSeeder");
+
+        $this->command->info("Tabla posts cargada");
 	}
 
+}
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table("users")->insert(array(
+                "email"     =>      "prueba@gmail.com",
+                "password"  =>      Hash::make("123456"),
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+        DB::table("users")->insert(array(
+                "email"     =>      "paco@gmail.com",
+                "password"  =>      Hash::make("123456"),
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+        DB::table("users")->insert(array(
+                "email"     =>      "julian@gmail.com",
+                "password"  =>      Hash::make("123456"),
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+    }
+}
+
+
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table("posts")->insert(array(
+                "user_id"   => 1,
+                "title"     => "título primer post de burasu",
+                "content"   => "Contenido del primer post",
+                "created_at"=> new DateTime,
+                "updated_at"=> new DateTime
+            )
+        );
+
+        DB::table("posts")->insert(array(
+                "user_id"   => 1,
+                "title"     => "título segundo post de burasu",
+                "content"   => "Contenido del segundo post",
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+
+        DB::table("posts")->insert(array(
+                "user_id"   => 2,
+                "title"     => "título tercer post de prueba",
+                "content"   => "Contenido del primer post",
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+
+        DB::table("posts")->insert(array(
+                "user_id"   => 3,
+                "title"     => "título cuarto post de 3",
+                "content"   => "Contenido del primer post",
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+
+        DB::table("posts")->insert(array(
+                "user_id"   => 4,
+                "title"     => "título quinto post de 4",
+                "content"   => "Contenido del primer post",
+                "created_at"=>      new DateTime,
+                "updated_at"=>      new DateTime
+            )
+        );
+
+    }
 }
